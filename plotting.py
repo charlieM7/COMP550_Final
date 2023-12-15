ee21a0602654d2aed92d13493f60ecd0392af79f
Jackie <jia.q.wu@mail.mcgill.ca>
2023-12-15 13:13:24 +0800

    updated scatter plot with option fr abs

--- plotting.py
+++ plotting.py
@@ -1,4 +1,49 @@
+import random
+
 import matplotlib.pyplot as plt
+
+# polarity = [0.5, 1, 0, 0.5, -1, -0.25]
+# engagement = [1, -5, 1, 2, 4, 5]
+
+# { polarity_score : engagement_score}
+positive_pol = []
+positive_eng = []
+
+neutral_pol = []
+neutral_eng = []
+
+negative_pol = []
+negative_eng = []
+
+
+def gen_rand_list(number, val):
+    my_list = []
+    for i in range(number):
+        my_list.append(val * random.random() - val/2)
+    return my_list
+
+
+def sort_pos_neu_neg(polarity, engagement):
+    for i in range(len(polarity)):
+        if polarity[i] >= 0.5:
+            positive_pol.append(polarity[i])
+            positive_eng.append(engagement[i])
+            # positive[polarity[i]] = engagement[i]
+        elif polarity[i] <= -0.5:
+            negative_pol.append(polarity[i])
+            negative_eng.append(engagement[i])
+            # negative[polarity[i]] = engagement[i]
+        else:
+            neutral_pol.append(polarity[i])
+            neutral_eng.append(engagement[i])
+            # neutral[polarity[i]] = engagement[i]
+
+
+def get_abs (list):
+    ans = []
+    for key in list:
+        ans.append(abs(key))
+    return ans
 
 
 def get_mean(polarity, engagement):
@@ -39,6 +84,25 @@ def scatter_plot(polarity, engagement):
     plt.show()
     # plt.savefig("scatter_plot.jpg")
 
+def scatter_plot_abs(polarity, engagement):
+    # convert neg value to positive
+    # now only check if polarity influence engagement
+
+    neu_x = get_abs(neutral_pol)
+
+    neg_x = get_abs(negative_pol)
+
+    plt.scatter(positive_pol, positive_eng, color="red", label="positive")
+    plt.scatter(neg_x, negative_eng, color="blue", label="negative")
+    plt.scatter(neu_x, neutral_eng, color="black", label="neutral")
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Scatter Plot 1')
+
+    plt.show()
+
+
 
 def bar_plot_means(polarity, engagement):
     means = get_mean(polarity, engagement)
@@ -58,7 +122,18 @@ def bar_plot_means(polarity, engagement):
 
 
 if __name__ == "__main__":
-    polarity = [0.5, 1, 0, 0.5, -1, -0.25]
-    engagement = [1, -5, 1, 2, 4, 5]
+    polarity = gen_rand_list(50, 2)
+    engagement = gen_rand_list(50, 10)
+
+    # polarity = [0.5, 1, 0, 0.5, -1, -0.25]
+    # engagement = [1, -5, 1.5, 2, 4, 5]
+    sort_pos_neu_neg(polarity, engagement)
+
+    print(len(positive_pol))
+    print(len(positive_eng))
+    print()
+    print(len(negative_pol))
+    print(len(negative_eng))
     scatter_plot(polarity, engagement)
     bar_plot_means(polarity, engagement)
+    scatter_plot_abs(polarity, engagement)
